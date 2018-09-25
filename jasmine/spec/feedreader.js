dbb83e294da6beec81a147440d1ff323866dc538
dnceglia <d.n.ceglia@hotmail.com>
2018-09-25 15:16:52 -0400

    creation of feed entries test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -53,31 +53,44 @@ $(function() {
 
         /* A test that ensures the menu element is
            hidden by default.*/
-
         it('is hidden by default', function() {
             expect(docBody.classList.contains('menu-hidden')).toBe(true);
         });
 
         /* A test that ensures the menu changes
         * visibility when the menu icon is clicked. */
-        
         it('alternates visibility after a click', function() {
+
+            // check that the first click the menu appears
             hamburger.click();
             expect(docBody.classList.contains('menu-hidden')).toBe(false);
+
+            // check that the second click the menu is hidden
             hamburger.click();
             expect(docBody.classList.contains('menu-hidden')).toBe(true);
         });
     });
-    
 
-    /* TODO: Write a new test suite named "Initial Entries" */
 
-        /* TODO: Write a test that ensures when the loadFeed
+    /* A new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
+        let feed = document.querySelector('.feed');
+
+        // creation of async beforeEach function
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                done();
+            });
+        });
+
+        /* A test that ensures when the loadFeed
          * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+         * a single .entry element within the .feed container. */
+        it('exist in the feed', function () {
+            expect(feed.children).toBeDefined();
+        });
+    });
+
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
